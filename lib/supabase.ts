0bd6686d8832b0cd41ff46b519913bf78decfe55
Michele Sannti <michelesannti@gmail.com>
2026-02-23 19:04:54 -0300

    remove supabase env throw

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function getSupabaseClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error("Supabase environment variables are missing.");
-  }
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
   return createClient(supabaseUrl, supabaseAnonKey);
 }
